812e7cfdb1eb8a7bea84a6184f269b471fd9d443
anettka166 <anna_radionova@yahoo.com>
2022-05-28 09:57:58 -0400

    Sauce-Labs-E2E-Final clean project

---
+++ src/test/java/com/academy/techcenture/UserLoginTest.java
@@ -0,0 +1,26 @@
+package com.academy.techcenture;
+
+import org.testng.annotations.Test;
+
+public class UserLoginTest {
+
+    @Test
+    public void loginValidCredentialsTest(){
+        System.out.println("Test Login Fanctionality");
+    }
+
+    @Test
+    public void loginWrongCredentialsTest(){
+        System.out.println("Test Login Fanctionality");
+    }
+
+    @Test
+    public void loginEmptyCredentialsTest(){
+        System.out.println("Test Login Fanctionality");
+    }
+
+    @Test
+    public void loginInValidCredentialsTest(){
+        System.out.println("Test Login Fanctionality");
+    }
+}
